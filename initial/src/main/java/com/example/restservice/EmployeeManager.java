725721fecaa03c98a21fdc7a203ab8333f322532
roberttumaliuan <roberttumaliuan@gmail.com>
2023-09-17 21:47:36 -0500

    Added Employee classes

---
+++ initial/src/main/java/com/example/restservice/EmployeeManager.java
@@ -0,0 +1,57 @@
+package com.example.restservice;
+
+import org.springframework.stereotype.Repository;
+// Importing the employees class to
+// use the defined properties
+// in this class
+import com.example.restservice.Employees;
+@Repository
+// Class to create a list
+// of employees
+public class EmployeeManager {
+    private static Employees list = new Employees();
+
+    // This static block is executed
+    // before executing the main
+    // block
+    static
+    {
+        // Creating a few employees
+        // and adding them to the list
+        list.getEmployeeList().add(
+                new Employee(
+                        1,
+                        "Prem",
+                        "Tiwari",
+                        "chapradreams@gmail.com",
+                        "Mr."
+                )
+        );
+
+        list.getEmployeeList().add(new Employee(
+                2, "Vikash",
+                "Kumar",
+                "abc@gmail.com", "Mr."
+        ));
+
+        list.getEmployeeList().add(new Employee(
+                3, "Ritesh",
+                "Ojha",
+                "asdjf@gmail.com", "Mr."
+        ));
+    }
+
+    // Method to return the list
+    public Employees getAllEmployees()
+    {
+        return list;
+    }
+
+    // Method to add an employee
+    // to the employees list
+    public void addEmployee(Employee employee)
+    {
+        list.getEmployeeList().add(employee);
+    }
+
+}
